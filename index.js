3438705973a4b7dfe80e0ee239e45653bc549cf3
Victor hugo cordeiro <victor.hugo@systemhope.com.br>
2020-01-17 14:11:58 -0300

    configuração e conexão com db

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express();
+
+mongoose.connect('mongodb+srv://vhcord:hager1417@cluster0-yzfmt.mongodb.net/week10?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.get('/', (request , response) => {
   return response.json({ message: 'vai logo com video porra'});
