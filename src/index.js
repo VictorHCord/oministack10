4a766c16ed1378655e494eb58439b681c48bea4b
Victor hugo cordeiro <victor.hugo@systemhope.com.br>
2020-01-29 23:04:22 -0300

    Atualização em requisições

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 const routes = require('./routes')
 const app = express();
 
 mongoose.connect('mongodb+srv://vhcord:hager1417@cluster0-yzfmt.mongodb.net/week10?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useFindAndModify: false
 });
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 app.listen(3333);
+mongoose.set('useCreateIndex' , true)
